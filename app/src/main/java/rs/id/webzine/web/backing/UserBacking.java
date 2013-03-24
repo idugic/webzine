c9fbb4de437b8597db20598c905d323602f8bfe3
Ivan Dugic <idugic@gmail.com>
2013-03-24 18:06:30 +0100

    user address

---
+++ app/src/main/java/rs/id/webzine/web/backing/UserBacking.java
@@ -0,0 +1,213 @@
+package rs.id.webzine.web.backing;
+
+import java.util.Date;
+
+import rs.id.webzine.domain.Address;
+import rs.id.webzine.domain.Roles;
+import rs.id.webzine.domain.UserStatus;
+
+public class UserBacking {
+
+	// user
+	private Integer userId;
+
+	private Roles roleId;
+
+	private UserStatus statusId;
+
+	private String userName;
+
+	private String password;
+
+	private String firstName;
+
+	private String lastName;
+
+	private Date birthday;
+
+	private byte[] image;
+
+	private String imageContentType;
+
+	private String imageUrl;
+
+	private Address addressId;
+
+	// address
+	private String email;
+
+	private String phone;
+
+	private String streetLine;
+
+	private String city;
+
+	private String postalCode;
+
+	private String country;
+
+	private String countryCode;
+
+	private String www;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Roles getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Roles roleId) {
+		this.roleId = roleId;
+	}
+
+	public UserStatus getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(UserStatus statusId) {
+		this.statusId = statusId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public String getImageContentType() {
+		return imageContentType;
+	}
+
+	public void setImageContentType(String imageContentType) {
+		this.imageContentType = imageContentType;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public Address getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Address addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getStreetLine() {
+		return streetLine;
+	}
+
+	public void setStreetLine(String streetLine) {
+		this.streetLine = streetLine;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getWww() {
+		return www;
+	}
+
+	public void setWww(String www) {
+		this.www = www;
+	}
+
+}
